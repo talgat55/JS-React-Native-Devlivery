a01d279ad823f8a00f9ceb17816c1d6413bed47c
talgat55 <lightx_design@yahoo.com>
2021-10-02 00:42:43 +0600

    add bottom bars

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
 
-export default function App() {
+import {Home,Restaurant,OrderDelivery} from './screens';
+
+const Stack = createStackNavigator();
+import Tabs from './navigation/tabs'
+const App = () =>{
   return (
-    <View style={styles.container}>
-      <Text>test</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+          <Stack.Navigator
+              screenOptions={{
+                headerShown: false
+              }}
+              initialRouteName={'Home'}
+          >
+            <Stack.Screen name="Home"  component={Tabs} />
+            <Stack.Screen name="Restaurant"  component={Restaurant} />
+            <Stack.Screen name="OrderDelivery"  component={OrderDelivery} />
+          </Stack.Navigator>
+      </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default  App;
---
+++ navigation/tabs.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+    View,
+    Image,
+    TouchableOpacity
+} from "react-native";
+import {createBottomTabNavigator, BottomTabBar} from "@react-navigation/bottom-tabs";
+import {Home} from '../screens';
+import {COLORS,icons} from "../static";
+const Tab = createBottomTabNavigator();
+
+const Tabs = () =>{
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({focused}) =>(
+                        <Image
+                            source={icons.cutlery}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={Home}
+                options={{
+                    tabBarIcon: ({focused}) =>(
+                        <Image
+                            source={icons.search}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+
+                        />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+export  default  Tabs;
