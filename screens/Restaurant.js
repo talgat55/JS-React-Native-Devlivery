4229dfd56a62e4a0c21c86decab5ab8ee7d2f90b
talgat55 <lightx_design@yahoo.com>
2021-10-01 23:38:02 +0600

    add boilerplates

---
+++ screens/Restaurant.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    View,
+    Text
+}  from "react-native";
+
+const Restaurant = () =>{
+
+    return (
+        <View>
+            <Text>
+                Restaurant
+            </Text>
+        </View>
+    )
+}
+export default  Restaurant;
