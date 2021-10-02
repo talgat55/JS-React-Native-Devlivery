d5535de0829e83414a6b8e124e4f96176c41dbf5
talgat55 <lightx_design@yahoo.com>
2021-10-02 15:09:14 +0600

    prepare Home screen

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
-    View,
     Image,
-    TouchableOpacity
 } from "react-native";
 import {createBottomTabNavigator, BottomTabBar} from "@react-navigation/bottom-tabs";
 import {Home} from '../screens';
